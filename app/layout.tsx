62317a8b9ed15d78d961e450c819984efb22fa15
josephaven <jr457944@gmail.com>
2026-01-14 20:49:16 -0600

    fix(next): type metadata export using next Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import type { Metadata } from "next";
 import "./globals.css";
 
-export const metadata = {
+export const metadata : Metadata = {
   title: "Joseph Avendaño | Software Engineer Portfolio",
   description: "Portfolio",
 };
